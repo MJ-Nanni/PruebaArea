b7eaad7d7b385d855bbad161ea330ee0e2fdc21e
MJ <110538262+MJ-Nanni@users.noreply.github.com>
2022-08-11 19:11:31 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+let base : num = 4
+let altura : num = 10
+let area : num = base * altura
+
+console.log (area);
+
+
+
+
+
